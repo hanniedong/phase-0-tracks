cd4a04ff4a4cecbf21ddac30f9cb6f4b4aa55cf3
Hannie Dong <hdong92@gmail.com>
2017-05-10 20:12:53 -0700

    Added detection logic.

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,15 +1,25 @@
 puts "What is your name?"
 name = gets.chomp
 
+if name == "Drake Cula" 
+	vampire_name = true
+elsif name == "Tu Fang"
+	vampire_name = true
+else
+	normal_name = true
+end
+	
 puts "How old are you?"
 age = gets.to_i
 
 puts "What year were you born?"
 year = gets.to_i
 
-	if 2017 - year = age
+	if 2017 - year == age
+		normal_age = true 
 		puts "Age and year of birth lines up mathematically"
 	else
+		vampires_age = true
 		puts "Age and year of birth does not line up mathematically"
 	end
 
@@ -17,25 +27,28 @@ puts "Our company cafeteria serves garlic bread. Should we order some for you? (
 garlic = gets.chomp
 
 if garlic == "y"
-	normal_eats_garlic = true
+	normal_garlic = true
 else 
-	vampires_eats_garlic = true
+	vampires_garlic = true
 end
 
 puts "Would you like to enroll into the company's health insurance? (y or n)"
 insurance = gets.chomp
+
 if insurance == "y"
-	normal_insurance = true
+	normaL_insurance = true
 else 
 	vampires_insurance = true
 end
 
-vampires_gets_age = false
-vampires_eat_garlic = false
-vampires_insurance = false
-normal_gets_age = true
-normal_eat_garlic = true
-normal_insurance = true
-
-
-
+if normal_age && (normal_garlic || normal_insurance)
+	puts “Probably not a vampire.”
+elsif vampires_age && (vampires_garlic || vampires_insurance) 
+	puts “Probably a vampire.”
+elsif vampires_age && vampires_garlic && vampires_insurance 
+	puts “Almost certainly a vampire.”
+elsif vampires_name
+	puts "Definitely a vampire."
+else
+	puts “Results inconclusive.”
+end
