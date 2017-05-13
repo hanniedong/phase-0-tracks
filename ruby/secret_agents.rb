b9a98c1fbd2aad9cef25b1c304e66f22695f5ce4
Hannie Dong <hdong92@gmail.com>
2017-05-13 15:05:13 -0700

    Wrote out algorithms for methods.

---
+++ ruby/secret_agents.rb
@@ -0,0 +1,21 @@
+=begin
+Create method encrypt with argument string
+Set index to 0 
+Create placeholder variable with ""
+Loop through each letter of the word until the last letter
+Use the .next method to advance every letter of a string one letter forward
+Have each letter add to the placeholder variable
+Print the final result
+end=
+
+=begin
+Create method encrypt with argument string
+Set index to 0 
+Create placeholder variable with ""
+Create variable containing "abcdefghijklmnopqrstuvwxyz"
+Loop through each letter of the word until the last letter
+Create a new string that contains the letter corresponding to the original string
+Find the index of that new string letter in the "abcdefghijklmnopqrstuvwxyz" string and subtract by 1
+Add each letter to the placeholder variable
+Print the final result
+end=
