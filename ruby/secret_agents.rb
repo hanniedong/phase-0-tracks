13dc4f000143b657ae4d9ace7f631be342453348
Hannie Dong <hdong92@gmail.com>
2017-05-13 15:25:16 -0700

    Tested program and tried nested call method.

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -7,11 +7,15 @@ Use the .next method to advance every letter of a string one letter forward
 Have each letter add to the placeholder variable
 Print the final result
 =end
+
 def encrypt(string)
 index = 0
 empty_string = ""
 	while index < string.length
-		empty_string = empty_string + string[index].next
+		if string[index] == "z"
+			empty_string = "a"
+		else empty_strisng = empty_string + string[index].next
+		end
 		index +=1
 	end
 p empty_string
@@ -43,4 +47,15 @@ empty_string = ""
 	end
 p empty_string
 end
+
+=begin
+DRIVER CODE
+encrypt("abc")
+encrypt("zed") 
+decrypt("bcd") 
+decrypt("afe") 
+
+Why does decrypt(encrypt("swordfish")) work? This nested call method works because the output of the 
+encrypt method provides the argument of the decrypt method. 
+=end
 
