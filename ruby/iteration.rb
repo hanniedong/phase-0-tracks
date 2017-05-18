5b2b6c9398fa1a824ba3d995cc49bbcc62ecf9ab
Hannie Dong <hdong92@gmail.com>
2017-05-18 16:34:25 -0700

    Aaded release 1

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -7,3 +7,28 @@ end
 
 dbc_pair {|name1, name2| puts "done by #{name1} and #{name2}"}
 
+#RELEASE 1
+myarray = ["apples", "oranges", "pear", "bananas"]
+
+myhash = {
+  fruit: "apples",
+  meat: "steak",
+  drink: "water",
+  dessert: "ice cream"
+}
+
+myarray.each do|fruit| 
+  puts "We need to buy #{fruit}"}
+end
+
+
+myhash.each do|cat, item| 
+  puts "for #{cat} we'll buy #{item}"}
+end
+
+
+myarray.map! do |fruit|
+  puts fruit
+  puts "10" + " #{fruit}" 
+end
+
