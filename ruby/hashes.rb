ca0548a1f4e87d7aabcb9f5e34d931969a4d1281
Hannie Dong <hdong92@gmail.com>
2017-05-18 15:24:27 -0700

    Added pseudocode

---
+++ ruby/hashes.rb
@@ -0,0 +1,15 @@
+=begin
+Prompt interior designer to input details of a given client including: 
+	client's name
+	age
+	number of children
+	decor theme
+	favorite color	
+Covert user input to appropriate data type
+Assign thee user input to a variable
+Create a hash which each symbol key to the variable
+Print the hash back when designer has answered all of the questions
+Give the user the opportunity to update the key 
+Assign the input to the key to update the volume
+Print the latest version of the hash
+=end
