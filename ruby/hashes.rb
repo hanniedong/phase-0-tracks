0008d752b262163a0269acbad9c13856a59d17c8
Hannie Dong <hdong92@gmail.com>
2017-05-18 15:27:18 -0700

    Added user input and hash.

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -13,3 +13,26 @@ Give the user the opportunity to update the key
 Assign the input to the key to update the volume
 Print the latest version of the hash
 =end
+
+puts "What is your name?"
+full_name = gets.chomp
+
+puts "What is your age?"
+age_provided = gets.to_i 
+
+puts "How many children do you have?"
+number_of_children = gets.to_i
+
+puts "What is your preferred decor theme?"
+decor_theme = gets.chomp
+
+puts "What is your favorite color?"
+favorite_color = gets.chomp
+
+response = {
+  name: full_name,
+  age: age_provided,
+  children: number_of_children,
+  theme: decor_theme, 
+  color: favorite_color
+}
