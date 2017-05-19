dfe0b1f0b7735f3650c7c8b14b8339dcbb4ddcd0
Hannie Dong <hdong92@gmail.com>
2017-05-18 17:11:24 -0700

    Designed and built a nested data structure.

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,31 @@
+# Grocery List 
+
+grocery_list = {
+  fruits: [
+    'apples', 
+    'oranges', 
+    'bananas' 
+    ],
+    vegetables: [
+    'onions', 
+    'carrots', 
+    'spinach'
+    ],
+  drinks: {
+      nonalcoholic: [
+        'water',
+        'juice'
+        ],
+      alcoholic: [
+        'tequila', 
+        'wine'
+        ],
+    },
+  meats: [
+    'chicken',
+    'steak'
+    ],
+  breakfast: [
+    'oatmeal',
+    'cereal'
+    ]
