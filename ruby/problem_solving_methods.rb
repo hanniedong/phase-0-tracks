6eba12cbd304951f7f7203fe7c4e8d4364958880
Hannie Dong <hdong92@gmail.com>
2017-05-22 07:50:59 -0700

    Bubble sort method.

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -34,3 +34,27 @@ end
 p fib (6)
 p fib(7)
 p fib (4)
+
+=begin
+Start off with array with numbers in random order
+Go through array starting at position 0 and 1
+Create method that takes two numbers next to each other and moves the bigger one to the right and the lesser one to the left 
+=end
+arr = [3, 2, 5, 1, 6, 7, -1, 1, -5, 2]
+
+def array_sort(array)
+  sorted = true
+  cycle = array.length - 1
+  while sorted == true
+    sorted = false
+    cycle.times {|index|
+      if array[index] > array[index+1]
+        array[index], array[index+1] = array[index+1], array[index]
+        sorted = true
+      end
+    }
+  end  
+  p array
+end
+
+p array_sort(arr)
