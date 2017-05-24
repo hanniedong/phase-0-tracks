d00cf8e5b0da49cccd5cb402ec3343ade93a7f02
Hannie Dong <hdong92@gmail.com>
2017-05-24 11:48:13 -0700

    Designed a puppy class.

---
+++ ruby/puppy.rb
@@ -0,0 +1,21 @@
+=begin
+
+PUPPY CLASS DESIGN
+
+SPECIES ------------------------
+Puppy
+
+CHARACTERISTICS ----------------
+Friendly: yes
+Adorable: yes 
+Name: varies
+Color: varies
+
+BEHAVIOR -----------------------
+Jump
+Drool
+Play Fetch
+Cuddle
+Eat
+
+=end
