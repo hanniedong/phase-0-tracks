da7438c1ff0d531823c92dc18ff345772c05b39f
Hannie Dong <hdong92@gmail.com>
2017-05-25 15:50:58 -0700

    Mixed in a module.

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,5 @@
+#RELEASE 1
+
 module Shout
 	def self.yell_angry(words)
 		words + "!!!" + " :("
@@ -10,3 +12,24 @@ end
 
 p Shout.yell_angry("WHAT")
 p Shout.yelling_happy("Yes!")
+
+#RELEASE 2
+module Flight
+  def take_off(altitude)
+    puts "Taking off and ascending until reaching #{altitude} ..."
+  end
+end
+
+class Bird
+	include Flight
+end
+
+class Plane
+	include Flight
+end
+
+bird = Bird.new
+bird.take_off(800)
+
+plane = Plane.new
+plane.take_off(30000)
