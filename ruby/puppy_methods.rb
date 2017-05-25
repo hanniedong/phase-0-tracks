4c586bf9a73c71fafef22ec23790ccfefa98a3b6
Hannie Dong <hdong92@gmail.com>
2017-05-24 17:26:12 -0700

    Added methods and driver code.

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,3 +1,5 @@
+#RELEASE 0+1
+
 class Puppy
 
   def fetch(toy)
@@ -5,5 +7,34 @@ class Puppy
     toy
   end
 
+  def speak(integer)
+  	integer.times {
+  		puts "Woof!" 
+  	}
+  end
+  
+  def roll_over 
+    puts "Roll over"
+  end
+  
+  def dog_years(age)
+    puts age * 7 
+  end
+  
+  def handshake 
+    puts "Extends paw."
+  end
+  
+  def initialize
+    puts "Initializing new puppy instance..."
+  end
 end
+
+fido = Puppy.new
+fido.fetch("ball")
+fido.speak(5)
+fido.roll_over
+fido.dog_years(6)
+fido.handshake
+
 
