58dce2de10edaacc726805691a3d6de8f19aa7fd
Hannie Dong <hdong92@gmail.com>
2017-06-02 16:35:28 -0700

    Created reverse function.

--- js/explore.js
+++ js/explore.js
@@ -4,5 +4,17 @@
 	//starting from the last position of the string, go through each letter one by one until the last letter is reached
 	//store letters into a new empty variable
 	//call function and store result of function into new variable 
-//output: reversed string
+//output: reversed stringfunction reverse(string){
+	
+function reverse(string){
+	var reversedWord = ""
+	for (var i = string.length - 1; i >= 0; i --){
+	reversedWord += string[i]
+  }
+  return reversedWord
+}
+var reversedInput = reverse("yellow")
+console.log(reversedInput)
+
+
 
