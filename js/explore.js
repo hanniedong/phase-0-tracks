cf667ddbece9c0de64e73c36767a5040d6e2e0de
Hannie Dong <hdong92@gmail.com>
2017-06-02 16:35:04 -0700

    Added pseudocode.

---
+++ js/explore.js
@@ -0,0 +1,8 @@
+//FUNCTION
+//input: string
+//steps: reverse string
+	//starting from the last position of the string, go through each letter one by one until the last letter is reached
+	//store letters into a new empty variable
+	//call function and store result of function into new variable 
+//output: reversed string
+
