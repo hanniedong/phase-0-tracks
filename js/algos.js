f7e34b83dca82c398f8d3d56a4f68be0bbcb6130
Hannie Dong <hdong92@gmail.com>
2017-06-05 08:28:28 -0700

    Created random test data generator

--- js/algos.js
+++ js/algos.js
@@ -40,18 +40,37 @@ function matchItems(object1, object2) {
 //Write a function that takes an integer for length, and builds and returns an array of strings of the given length. 
 //The words should be of randomly varying length, with a minimum of 1 letter and a maximum of 10 letters.
 //INPUT: Integer
-//STEPS: Build an array based on the integer number
+//STEPS: Build an array containing integer number of words
+	//randomly select 1-10 to create the words
+	//using this, create a new string of words generating random letters from a master alpabet string
 //OUTPUT: array with x amount of words with a min of 1 letter and a max of 10 letter
  
+function randomWords(integer){
+	var alphabet = "abcdefghijklmnopqrstuvwxyz";
+ 	var newWord = " ";
+ 	var wordArray = [];
+ 	min = 1;
+  	max = 10;
+  
+  for (var j = 0; j < integer; j ++){
+  var randomWord ="";
+    lengthOfWord =  Math.floor(Math.random() * (max - min + 1)) + min
+    for (var i = 0; i < lengthOfWord; i++) {
+      	randomWord += (alphabet[Math.round(Math.random() * 25)]);
+      }
+     wordArray.push(randomWord)
+  }
+  return wordArray;
+}
 
 
-
- 
  //DRIVER
  //RELEASE 0
  words = ["banana","apple","fig"];
  console.log(findLongestWord(words));
  //RELEASE 1
- var person1 = {name: "Bobby", age: 13};
- var person2 = {name: "Jimmy", age: 13};
+ var person1 = {name: "Hannie", age: 25};
+ var person2 = {name: "Hannie", age: 13};
  console.log(matchItems(person1, person2));
+ //RELEASE 2
+console.log(randomWords(7))
