ee16e55d667cfa54e74b34b39d5f212b350b25a9
Hannie Dong <hdong92@gmail.com>
2017-06-05 06:11:19 -0700

    Built constructors.

--- js/data_structures.js
+++ js/data_structures.js
@@ -8,4 +8,16 @@ var horse = {};
 for (var i = 0; i < color.length; i++){
 	horse[name[i]]= color[i]
 }
+console.log(horse)
+
+function car_assembly(color, type, spare_needed){
+  this.color = color;
+  this.type = type;
+  this.spare_needed = spare_needed
+  this.ignition = function () {console.log("vroom vroom vroom")};
+  console.log("generating a new "+color+ " " + type + " with "+ spare_needed + " spare parts")
 }
+
+var first_car = new car_assembly("blue", "sedan", 4);
+console.log(first_car)
+first_car.ignition()
