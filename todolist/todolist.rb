d6175ef2ec89c7461744360ea792691bd9cd4025
Hannie Dong <hdong92@gmail.com>
2017-06-09 10:26:44 -0700

    Fixed user interface and added view method.

--- todolist/todolist.rb
+++ todolist/todolist.rb
@@ -1,5 +1,4 @@
 require 'SQLite3'
-require 'faker'
 
 db = SQLite3::Database.new("todolist.db")
 db.results_as_hash = true
@@ -17,57 +16,75 @@ db.execute(create_task_list)
 
 def display_list(db)
 	list = db.execute("SELECT * FROM tasks")
+	puts "---------------------------------------------------------------"
 	list.each do |row|
 		puts "Task: #{row['task']} | Description: #{row['description']} | Completed: #{row['completed']}"
 	end
+	puts "---------------------------------------------------------------"
 end
 
-def update_task(db)
-	puts "What task would you like to mark as complete?"
-	input_update = gets.chomp
-	db.execute("UPDATE tasks SET completed='yes' WHERE task='#{input_update}'")
+def update_task(update, db)
+	db.execute("UPDATE tasks SET completed='yes' WHERE task='#{update}'")
 end
 
-def add_task(db)
-	puts "Add the task."
-	input_task = gets.chomp
-	puts "Provide a description for the task."
-	input_description = gets.chomp
-	db.execute("INSERT INTO tasks(task, description, completed) VALUES ('#{input_task}', '#{input_description}', 'no')")
+def add_task(task, description, db)
+	db.execute("INSERT INTO tasks(task, description, completed) VALUES ('#{task}', '#{description}', 'no')")
 end 
 
-def delete_task(db)
-	puts "What task would you like to delete?"
-	input_delete = gets.chomp
-	db.execute("DELETE FROM TASKS WHERE task='#{input_delete}'")
+def delete_task(delete, db)
+	db.execute("DELETE FROM TASKS WHERE task='#{delete}'")
+end
+
+def view_task (view, db)
+	single_task = db.execute("SELECT * FROM tasks WHERE task='#{view}'")
+	puts "---------------------------------------------------------------"
+	single_task.each do |row|
+		puts "Task: #{row['task']} | Description: #{row['description']} | Completed: #{row['completed']}"
+	puts "---------------------------------------------------------------"
+	end
 end
 
 #Driver code
 puts "Welcome to the Task Tracker!"
+puts "Here is your current list:"
+puts display_list(db)
+puts "---------------------------------------------------------------"
 puts "Type ADD if you would like to add a task"
 puts "Type SEE if you would like to see updated list."
 puts "Type UPDATE if you would like to mark task as complete."
+puts "Type VIEW if you would like to view single task."
 puts "Type DELETE if you would like to delete task."
 puts "Type EXIT when finished"
+puts "---------------------------------------------------------------"
 loop do	
-puts "Would you like to ADD, UPDATE, DELETE, SEE list, or EXIT?"
+puts "Would you like to ADD task, UPDATE task as completed, DELETE task, VIEW single task, SEE list, or EXIT?"
 answer_task = gets.chomp
 	if answer_task == "ADD"
-		updated_task = add_task(db)
-		display_list(db)
+		puts "Add the task."
+		input_task = gets.chomp.gets.chomp.upcase!
+		puts "Provide a description for the task."
+		input_description = gets.chomp
+		add_task(input_task, input_description, db)
 	elsif answer_task == "UPDATE"
-		completed_task = update_task(db)
-		display_list(db)
+		puts "What task would you like to mark as complete?"
+		input_update = gets.chomp
+		update_task(input_update, db)
+	elsif answer_task == "VIEW"
+		puts "What task would you like to view?"
+		input_view = gets.chomp
+		view_task(input_view, db)
 	elsif answer_task == "DELETE"
-		delete_task (db)
-		display_list(db)
+		puts "What task would you like to delete?"
+		input_delete = gets.chomp
+		delete_task(input_delete, db)
 	elsif answer_task == "SEE"
 		display_list(db)
-		break
 	elsif answer_task == "EXIT" 
-		display_list(db)
 		break
 	else
 		puts "Please provide valid input"
 	end
 end
+
+puts "Here is your updated to do list!"
+display_list(db)
